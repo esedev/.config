1208237ff1e9b7c8eb3007543d923ac25fce7cb1
Seginus <ese.dev@gmail.com>
2026-02-22 12:04:50 +0300

    tune terminator

--- mtt-editor.sh
+++ mtt-editor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command -v emax 2>&1 >/dev/null; then
-    emax -nw "$@"
+    env EMAX_THM=Retro emacs -nw "$@"
 elif command -v mcedit 2>&1 > /dev/null; then
     mcedit "$@"
 else
