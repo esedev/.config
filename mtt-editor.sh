be2072bb15e7e02b691b92faaebbc60d038b1860
Seginus <ese.dev@gmail.com>
2026-02-26 23:24:59 +0300

    [*] use emacs daemon as default terminal editor

--- mtt-editor.sh
+++ mtt-editor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if command -v emax 2>&1 >/dev/null; then
-    env EMAX_THM=Retro emacs -nw "$@"
+    emax -nw -s mtt-edit "$@" || \
+        (emax --daemon=mtt-edit && emax -nw -s mtt-edit "$@")
+#    env EMAX_THM=None EMAX_PRF=devel emacs -nw "$@"
 elif command -v mcedit 2>&1 > /dev/null; then
     mcedit "$@"
 else
